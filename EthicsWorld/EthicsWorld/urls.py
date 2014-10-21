aa5b2355490f75e9cf75c9c12e12b33082645919
Dylan Johnson <dylanatasmsa@gmail.com>
2014-10-21 01:45:39 -0500

    Test of view for subapps

--- EthicsWorld/EthicsWorld/urls.py
+++ EthicsWorld/EthicsWorld/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'EthicsWorld.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'EthicsWorld.views.index', name = 'index'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^forum/', include('Apps.forum.urls')),
 )
