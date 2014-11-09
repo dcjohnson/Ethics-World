3dbb0a1af05a96733f137ed7a692dde8c55ef954
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-09 11:19:03 -0600

    Finished poll app are reset the database

--- EthicsWorld/Apps/poll/urls.py
+++ EthicsWorld/Apps/poll/urls.py
@@ -5,5 +5,7 @@ from Apps.poll import views
 urlpatterns = patterns('',
     url(r'^$', views.Index, name = 'index'),
     url(r'^indivpoll/$', views.IndividualPoll, name = 'indivpoll'),
-    url(r'^newpoll/$', views.MakePoll, name = 'makepoll')
+    url(r'^newpoll/$', views.MakePoll, name = 'makepoll'),
+    url(r'^newpollresponse/$', views.GetPollResponse, name = 'newpollresponse'),
+    url(r'^pollstats/$', views.RenderPollStats, name = 'pollstats')
 )
--- EthicsWorld/Apps/poll/views.py
+++ EthicsWorld/Apps/poll/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from Apps.poll.models import Question, AvaliableAnswers, Answer
 from common import helpers
+from django.db.models import Count
+from itertools import chain
 
 def Index(request):
     pollData = Question.objects.all()
@@ -38,4 +40,29 @@ def MakePoll(request):
             newAnswerModel.avaliableAnswersQuestionHash = questionHash
             newAnswerModel.save()
     finally:
+        return HttpResponseRedirect(reverse("poll:index"))
+
+def RenderPollStats(request):
+    try:
+        responses = Answer.objects.filter(answerQuestionHash = request.GET['questionhash']).values('answerChoiceHash').annotate(total = Count('answerChoiceHash')).order_by('answerChoiceHash')
+        avaliableAnswers = AvaliableAnswers.objects.filter(avaliableAnswersQuestionHash = request.GET['questionhash']).order_by('avaliableAnswersHash')
+        responseCount = len(Answer.objects.filter(answerQuestionHash = request.GET['questionhash']))
+        responseData = zip(responses, avaliableAnswers)
+        htmlData = {
+            'responsesCount':responseCount,
+            'responseData':responseData
+        }
+        return render(request,"poll/pollstats.html", htmlData)
+    except:
+        return HttpResponseRedirect(reverse("poll:index"))
+
+def GetPollResponse(request):
+    try:
+        newAnswer = Answer()
+        newAnswer.answerIPOfAnswerer = request.META['REMOTE_ADDR']
+        newAnswer.answerChoiceHash = request.POST['pollanswer']
+        newAnswer.answerQuestionHash = request.POST['questionhash']
+        newAnswer.save()
+        return HttpResponseRedirect(reverse("poll:pollstats") + "?questionhash=" + request.POST['questionhash'])
+    except:
         return HttpResponseRedirect(reverse("poll:index"))
